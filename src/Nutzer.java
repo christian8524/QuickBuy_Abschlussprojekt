1e451ffef27786ba09d82c9633c7487ea16646e7
0Raef <4113@htl.rennweg.at>
2026-06-03 11:48:14 +0200

    bisschen Logik gemacht

--- src/Nutzer.java
+++ src/Nutzer.java
@@ -10,4 +10,40 @@ public class Nutzer {
         this.guthaben = guthaben;
     }
 
+    public float getGuthaben() {
+        return guthaben;
+    }
+
+    public void setGuthaben(float guthaben) {
+        this.guthaben = guthaben;
+    }
+    public void addToCart(Product p) {
+        warenkorb.add(p);
+    }
+
+    public float getTotal() {
+        float sum = 0;
+        for (Product p : warenkorb) {
+            sum += p.getPreis();
+        }
+        return sum;
+    }
+    public void clearCart() {
+        warenkorb.clear();
+    }
+    public boolean pay() {
+        float total = getTotal();
+
+        if (total > guthaben) {
+            System.err.println("Nicht genug Guthaben");
+            return false;
+        }
+
+        guthaben -= total;
+        clearCart();
+        return true;
+    }
+    public ArrayList<Product> getWarenkorb() {
+        return warenkorb;
+    }
 }
